ce03775e60d1e63e81634e672274ce780392186d
Ciornei Daniel <amseiden.mule@gmail.com>
2023-10-29 17:52:56 +0200

    One more commit.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿/*
+ 
+ Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10).
+ If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; 
+ otherwise, display the 3 smallest numbers in the list.
+ 
+ */
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
